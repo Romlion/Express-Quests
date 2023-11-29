7c515245a79eeb8769babab1f873a5227ff43d0d
Romain <98345704+Romlion@users.noreply.github.com>
2023-11-29 15:50:34 +0100

    PUT is not realy easy

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -41,8 +41,28 @@ const postUser = (req, res) => {
       res.sendStatus(500);
     });
 };
+
+const updateUser = (req, res) => {
+  const {
+    firstname, lastname, email, city, language,
+  } = req.body;
+
+  database.query('update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?', [firstname, lastname, email, city, language])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
 module.exports = {
   getUsers,
   getUserById,
   postUser,
+  updateUser,
 };
